6c40a5c7ab63152a9c87f6095f80fd85a48a396b
hanifnabila <hanifnabila10@gmail.com>
2023-02-22 19:00:55 +0700

    first commit

---
+++ resources/views/homePage.blade.php
@@ -0,0 +1,115 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+</head>
+
+<body>
+
+    <div class="container">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand ml-5 font-weight-bold" href="#">Dapur CAK HANIF</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                <div class="navbar-nav border rounded mr-5">
+                    <a class="nav-item nav-link active" href="#">Makanan <span
+                            class="sr-only">(current)</span></a>
+                    <a class="nav-item nav-link" href="#">Minuman</a>
+                    <a class="nav-item nav-link" href="#">Snack</a>
+                </div>
+            </div>
+        </nav>
+    </div>
+
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col">
+                <h1>Indonesian Regional Food</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa tempora, numquam, ipsum maxime ex
+                    cupiditate alias ut eius quam quia repellendus tenetur optio asperiores, nostrum libero vitae
+                    consectetur vero assumenda.</p>
+            </div>
+            <div class="col">
+                <img src="/img/soto.png" alt="">
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row mt-5 border ">
+            <div class="col-4 mt-4">
+                <img src="img/mi.png" class="img-thumbnail border-0" alt="">
+            </div>
+            <div class="col-8 mt-5">
+                <h3 class="text-center">Mi Aceh</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, vero earum quis rerum placeat quas
+                    mollitia velit ipsa tempora nihil cupiditate quam dignissimos veritatis voluptate vel dolores
+                    laudantium? Dolor totam deserunt fugit culpa quam labore, at maxime laborum ducimus consequatur
+                    fugiat consequuntur magni tempore rem inventore architecto mollitia hic assumenda.</p>
+            </div>
+        </div>
+    </div>
+
+    <hr>
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="img/soto.jpeg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Soto Lamongan</h5>
+                        <p class="card-text">Soto lamongan biasanya berisi potongan ayam yang disuai</p>
+                        <a href="#" class="btn btn-primary">Detail</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="img/soto.jpeg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Soto Lamongan</h5>
+                        <p class="card-text">Soto lamongan biasanya berisi potongan ayam yang disuai</p>
+                        <a href="#" class="btn btn-primary">Detail</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card" style="width: 18rem;">
+                    <img class="card-img-top" src="img/soto.jpeg" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Soto Lamongan</h5>
+                        <p class="card-text">Soto lamongan biasanya berisi potongan ayam yang disuai</p>
+                        <a href="#" class="btn btn-primary">Detail</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
